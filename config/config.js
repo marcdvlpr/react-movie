1a172c4f02dc4a97ab5e81e970cb74dcd6709275
Marc Dos Santos <mdossantosdev@gmail.com>
2020-02-13 17:12:16 +0100

    :wrench: Add config.js

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const dbCluster = process.env.DB_CLUSTER;
+const dbName = process.env.DB_NAME;
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+const db = `mongodb+srv://${dbUsername}:${dbPassword}@${dbCluster}/${dbName}?retryWrites=true&w=majority`
+const jwtSecret = process.env.JWT_SECRET;
+
+module.exports = {
+  db,
+  jwtSecret
+};
--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('API Running');
-})
+});
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} 🚀`);
 });
