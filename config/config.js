c6676fcd7679edbeeb7684ed117b83f90c720b2c
Marc Dos Santos <mdossantosdev@gmail.com>
2020-02-13 18:50:38 +0100

    :wrench: Add database config

--- config/config.js
+++ config/config.js
@@ -7,9 +7,5 @@ const dbName = process.env.DB_NAME;
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 const db = `mongodb+srv://${dbUsername}:${dbPassword}@${dbCluster}/${dbName}?retryWrites=true&w=majority`
-const jwtSecret = process.env.JWT_SECRET;
 
-module.exports = {
-  db,
-  jwtSecret
-};
+module.exports = { db };
---
+++ config/database.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { db } = require('./config');
+
+const connectDB = async () => {
+  const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  };
+
+  try {
+    await mongoose.connect(db, options);
+
+    console.log('Connected to the DB 🛰');
+  } catch(err) {
+    console.error(err.message)
+  }
+};
+
+module.exports = connectDB;
