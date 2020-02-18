6336c6a7846b461ed6eaf262038dc0a9959b7ad1
Marc Dos Santos <mdossantosdev@gmail.com>
2020-02-18 11:29:55 +0100

    :heavy_plus_sign: Add bcrypt dependency

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,14 +3,40 @@ const router = express.Router();
 const { registerValidation } = require('../validators');
 const { validationResult } = require('express-validator');
 
+const User = require('../models/User');
+
 router.post('/signup',
   registerValidation(),
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send(req.body)
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      // Encrypt password
+
+      // Return jsonwebtoken
+
+    } catch (err) {
+      console.error(err.message);
+      
+    }
+
   }
 );
 
