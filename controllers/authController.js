29abcd66f744fc149b76a8407169b56723cb07b1
Marc Dos Santos <mdossantosdev@gmail.com>
2020-02-19 19:16:56 +0100

    :sparkles: Add login function to authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,5 +49,43 @@ exports.register = async (req, res) => {
     );
   } catch (err) {
     console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid Credentials'}] });
+    }
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(
+      payload,
+      jwtSecret,
+      { expiresIn: '24h'},
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    )
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
   }
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const { registerValidation } = require('../validators');
-const { register } = require('../controllers/authController');
+const { register, login } = require('../controllers/authController');
 
 router.post('/register', registerValidation(), register);
+
+router.post('/login', login);
 
 module.exports = router;
