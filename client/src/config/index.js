289a77b37a359d3d620c91367970cdf9443f36da
Marc Dos Santos <mdossantosdev@gmail.com>
2020-02-27 13:24:35 +0100

    :wrench: Add config file

---
+++ client/src/config/index.js
@@ -0,0 +1,7 @@
+const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+// Sizes: w300, w780, w1280, original
+const BACKDROP_SIZE = 'w1280';
+// Sizes: w92, w154, w185, w342, w500, w780, original
+const POSTER_SIZE = 'w500';
+
+export { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };
