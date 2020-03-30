941f7552c4424c42a60763d0e0d2cfdf47f991e8
Marc Dos Santos <mdossantosdev@gmail.com>
2020-03-30 18:22:06 +0200

    :art: Add serve static assets to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
@@ -21,8 +22,14 @@ app.use(express.json());
 // Define Routes
 app.use('/api/v1', routes)
 
-app.get('/', (req, res) => {
-  res.send('API Running');
-});
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static fodler
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
