eecaa1613f0510c779dd821485a227c039f5c90d
Marc Dos Santos <mdossantosdev@gmail.com>
2021-06-18 10:24:03 +0200

    Add getMovie() to api in client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
+import { BASE_URL } from '../config';
 
 export const getMovies = async (searchTerm, page) => {
   const endpoint = searchTerm
     ? `${BASE_URL}/search/movie?query=${searchTerm}&page=${page}`
     : `${BASE_URL}?page=${page}`;
+  const { data } = await axios.get(endpoint);
+  return data;
+};
+
+export const getMovie = async (movieID) => {
+  const endpoint = `${BASE_URL}${movieID}`;
   const { data } = await axios.get(endpoint);
   return data;
 };
