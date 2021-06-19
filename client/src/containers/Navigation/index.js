d9386bff82042a164e0bd5d0eda09be89c1bcd2c
Marc Dos Santos <mdossantosdev@gmail.com>
2021-06-19 14:36:05 +0200

    Add HomeLink styled to Navigation component

--- client/src/containers/Navigation/index.js
+++ client/src/containers/Navigation/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from '@reach/router';
-import { StyledNavigation, StyledNavigationContent, Text } from './style';
+import { StyledNavigation, StyledNavigationContent, HomeLink, Text } from './style';
 
 const Navigation = ({ movie }) => {
   return (
     <StyledNavigation>
       <StyledNavigationContent>
         <Link to={'/'}>
-          <Text>Home</Text>
+          <HomeLink>Home</HomeLink>
         </Link>
         <Text>|</Text>
         <Text>{movie}</Text>
--- client/src/containers/Navigation/style.js
+++ client/src/containers/Navigation/style.js
@@ -12,17 +12,34 @@ export const StyledNavigation = styled.div`
 export const StyledNavigationContent = styled.div`
   display: flex;
   max-width: 1280px;
+  width: 100%;
   margin: 0 auto;
   padding: 0 2vw;
-  width: 100%;
+`;
+
+export const HomeLink = styled.p`
+  font-size: 2.5vh;
+  font-family: 'Abel', sans-serif;
+  color: rgb(255, 255, 255);
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    color: rgb(251, 197, 0);;
+    transform: scale(1.1);
+  }
+
+  @media screen and (max-width: 768px) {
+    font-size: 2vh;
+  }
 `;
 
 export const Text = styled.p`
-  font-family: 'Abel', sans-serif;
   font-size: 2.5vh;
+  font-family: 'Abel', sans-serif;
   color: rgb(255, 255, 255);
-  padding-right: .5vw;
   text-decoration: none;
+  margin-left: 1vh;
 
   @media screen and (max-width: 768px) {
     font-size: 2vh;
